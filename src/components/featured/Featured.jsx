c0043fa48bd80b44c1bf74efccd724fcf8ddbffc
chronorus <chronorus@gmail.com>
2022-12-30 08:41:10 +0700

    update: add dashboard

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,23 @@
+import './featured.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize='small' />
+      </div>
+      <div className="bottom">
+        test
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
