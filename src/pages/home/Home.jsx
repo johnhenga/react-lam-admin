5ed15812c23adba00834855c7441f1fa46a5f20d
John Heng <john@gosoft.co.th>
2022-12-31 09:35:56 +0000

    update: add pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import List from '../../components/table/Table'
 import Chart from '../../components/chart/Chart'
 import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
@@ -19,7 +20,11 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          <Chart />
+          <Chart title='Last 6 Months (Revenue)' aspect={2/1}/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List/>
         </div>
       </div>
     </div>
